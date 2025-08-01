5aefd5f0c3133bf8f648070d83ae5ee4aaf252a9
Yassir Choujai <113991646+yassirchoujai19@users.noreply.github.com>
2025-08-01 15:32:10 +0100

    Fix blank page issue

--- src/App.vue
+++ src/App.vue
@@ -158,7 +158,7 @@ const {
 } = useImageProcessor()
 </script>
 
-<style lang="scss">
+<style>
 @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap');
 
 .crop-master-app {
